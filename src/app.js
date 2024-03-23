cb481706dd5902b63b19041ba18ec9050515ae29
dopa <ISPQ@bk.ru>
2024-03-23 14:51:14 +0200

    first

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+
+const Div = styled.div`
+	margin: 200px;
+	color: white;
+	font-size: 50px;
+` // вызвали стилизованный компонент с тегом div (функцию объекта styled)
+
+export const App = () => {
+	return (
+		<Div>
+			<i className="fa-solid fa-user"></i>
+			<i className="fa-brands fa-github-square"></i>
+			<div>hello</div>
+		</Div>
+	)
+}
